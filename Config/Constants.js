205e9ffd65085cc2f4b271c86e670143614b6bff
TusharJavaDev <58426370+TusharJavaDev@users.noreply.github.com>
2021-03-31 17:47:14 +0530

    Supplier id change

--- Config/Constants.js
+++ Config/Constants.js
@@ -1,4 +1,4 @@
-export const supplierId = java.util.Random@2aa271dc;
+export const supplierId = java.util.Random@2dd8f082;
 
 // const staggingStoreManager =
 //   'http://ec2-13-126-234-238.ap-south-1.compute.amazonaws.com/';
